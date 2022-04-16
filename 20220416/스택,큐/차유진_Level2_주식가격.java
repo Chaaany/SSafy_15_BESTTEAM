1cd9995bb5c67d186352f576cfbabf234988f505
Yoojin Cha <55440076+yoojincha@users.noreply.github.com>
2022-04-16 11:42:15 +0900

    Create 차유진_Level2_주식가격.java

---
+++ 20220416/스택,큐/차유진_Level2_주식가격.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length; i++) {
+            int cnt = 0;
+            for(int j = i+1; j < prices.length; j++) {
+                if(prices[i] > prices[j]) {
+                    cnt++;
+                    break;
+                }
+                cnt++;
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+}
