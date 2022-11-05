6c1bdab97953e60458080f19856526f804716f48
Yoojin Cha <55440076+yoojincha@users.noreply.github.com>
2022-11-05 11:26:01 +0900

    Create 차유진_G3_23295_스터디시간정하기1.java

---
+++ 20221105/백준/차유진_G3_23295_스터디시간정하기1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main_G3_23295_스터디시간정하기1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int T = sc.nextInt();
+		int[] timeline = new int[100001];
+		int latest = 0;
+		for (int i = 0; i < N; i++) {
+			int K = sc.nextInt();
+			for (int j = 0; j < K; j++) {
+				int s = sc.nextInt();
+				int e = sc.nextInt();
+				latest = Math.max(latest, e);
+				for (int k = s; k < e; k++) {
+					timeline[k]++;
+				}
+				
+			}
+		}
+		int al = 0;
+		int ar = al+T;
+		int l = 0;
+		int r = l+T;
+		int happiness = 0;
+		int t = happiness;
+		while (r < latest) {
+			t = t - timeline[l] + timeline[r];
+			if(t > happiness) {
+				happiness = t;
+				al = l+1;
+				ar = r+1;
+			}
+			l++;
+			r++;
+			
+		}
+		System.out.println(al+" "+ar);
+	}
+}
