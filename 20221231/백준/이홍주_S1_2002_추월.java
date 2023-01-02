b53e305f91530cff2d4fe03cf3880c1222ba1626
Lee Hongjoo <65024497+Hongjoo22@users.noreply.github.com>
2023-01-02 16:53:09 +0900

    Create 이홍주_S1_2002_추월.java

---
+++ 20221231/백준/이홍주_S1_2002_추월.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_2002_추월 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		ArrayList<String> list = new ArrayList<>();
+		
+		for (int i = 1; i <= N; i++) {
+			String str = br.readLine().trim();
+			map.put(str, i);
+			list.add(str);
+		}
+		
+		int count = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			String now = br.readLine().trim();
+			
+			if(!list.get(0).equals(now)) count++;
+			list.remove(now);
+		}
+		
+		System.out.println(count);
+	}
+}
