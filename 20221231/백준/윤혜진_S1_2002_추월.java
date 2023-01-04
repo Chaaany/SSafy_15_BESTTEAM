abd60f2fd24e34677c96f10af1e8a5b38cc93f94
yyhjin <99111030+yyhjin@users.noreply.github.com>
2023-01-05 01:10:04 +0900

    Update 윤혜진_S1_2002_추월.java

--- 20221231/백준/윤혜진_S1_2002_추월.java
+++ 20221231/백준/윤혜진_S1_2002_추월.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -8,36 +10,45 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-		
+
 		String[] enter = new String[N];
-		String[] out = new String[N];
+		Map<String, Integer> out = new HashMap<>();
 		
 		for (int i = 0; i < N; i++) {
-			enter[i] = br.readLine();
+			String car = br.readLine();
+			enter[i] = car;
 		}
 		
 		for (int i = 0; i < N; i++) {
-			out[i] = br.readLine();
+			String car = br.readLine();
+			out.put(car, out.getOrDefault(car, i));
 		}
 		
 		int count = 0;
-		boolean[] v = new boolean[N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(!enter[i].equals(out[j])) {
-					if(!v[j]) {
-						v[j] = true;
-						count++;
-					}
-				}
-				else {
-					v[j] = true;
-					break;	
+				
+		for (String outKey : out.keySet()) {
+			int start = -1;  // 들어온 순서
+			int end = out.get(outKey);  // 나간 순서
+			
+			for (int i = 0; i < N; i++) {
+				if(enter[i].equals(outKey))
+					start = i;
+			}
+			
+			// 앞에 있던 차가 뒤로 갔으면 추월한 것
+			for (int i = 0; i < start; i++) {
+				int otherEnd = out.get(enter[i]);
+				
+				if(otherEnd > end) {
+					count++;
+					break;
 				}
 			}
+			
 		}
 		
 		System.out.println(count);
+
 	}
 
 }
