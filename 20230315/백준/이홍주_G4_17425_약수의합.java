9086b09333c34be7362ca75be79219698f576dc0
Lee Hongjoo <65024497+Hongjoo22@users.noreply.github.com>
2023-03-16 17:59:57 +0900

    Create 이홍주_G4_17425_약수의합.java

---
+++ 20230315/백준/이홍주_G4_17425_약수의합.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class BJ_17425_약수의합 {
+
+	static final int MAX = 1000000;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		long[] dp = new long[MAX+1];
+		long[] sum = new long[MAX+1];
+		
+		for (int i = 1; i <= MAX; i++) {
+			dp[i] += i;
+			for (int j = 2; j <= MAX/i; j++) {
+				if(i*j > MAX) break;
+				dp[i*j] += i;
+			}
+			sum[i] = dp[i]+sum[i-1];
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int t = 0; t < T; t++) {
+			int N = Integer.parseInt(br.readLine());
+		
+			sb.append(sum[N]+"\n");
+		}
+		System.out.print(sb.toString());
+	}
+
+}
