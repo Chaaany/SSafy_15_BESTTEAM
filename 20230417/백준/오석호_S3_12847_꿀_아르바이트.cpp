a5ac41c62776e1bd5300f1f23cec9285cafed2b4
Ssoco <dlscjs8646@gmail.com>
2023-04-22 03:31:58 +0900

    Create 오석호_S3_12847_꿀_아르바이트.cpp

---
+++ 20230417/백준/오석호_S3_12847_꿀_아르바이트.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+	cout.tie(NULL);
+
+	// 12847 꿀 아르바이트
+	// 누적합
+	int m, n;
+	int arr[100001] = { 0 };
+	int ans = 0;
+	cin >> m >> n;
+	for (int i = 0; i < m; ++i) cin >> arr[i];
+	int tmp = 0;
+	for (int i = 0; i < m; ++i) {
+		if (i >= n) tmp -= arr[i - n];
+		tmp += arr[i];
+		ans = max(ans, tmp);
+	}
+	cout << ans;
+
+	return 0;
+}
