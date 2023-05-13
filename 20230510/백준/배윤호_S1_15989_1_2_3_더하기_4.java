99f96747cfb49944aecbb70840964ea962f5714a
NKLCBAE <87966378+NKLCBAE@users.noreply.github.com>
2023-05-13 09:32:08 +0900

    Create 배윤호_S1_15989_1_2_3_더하기_4.java

---
+++ 20230510/백준/배윤호_S1_15989_1_2_3_더하기_4.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        for (int i = 4; i < 10001; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            int temp = Integer.parseInt(br.readLine());
+            sb.append(dp[temp][1] + dp[temp][2] + dp[temp][3]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
