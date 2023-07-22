580c2f189743491d5a2aa5e0177e6173c60e04b5
NKLCBAE <87966378+NKLCBAE@users.noreply.github.com>
2023-07-22 10:20:59 +0900

    Create 배윤호_S5_25496_장신구_명장_임스.java

---
+++ 20230722/백준/배윤호_S5_25496_장신구_명장_임스.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_25496_AccessoryMeisterIms {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int P = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int ans = 0;
+		while (P < 200 && ans < N) {
+			P += arr[ans++];
+		}
+		System.out.println(ans);
+	}
+
+}
