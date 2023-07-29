7c842dfd02445840be117a460a2b0a479903ffc7
NKLCBAE <87966378+NKLCBAE@users.noreply.github.com>
2023-07-29 10:52:40 +0900

    Create 배윤호_S2_18353_병사_배치하기.java

---
+++ 20230729/백준/배윤호_S2_18353_병사_배치하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_18353_SoldierArrange {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			arr[i] = num;
+		}
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] > arr[i]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			ans = Math.max(ans, dp[i]);
+		}
+		System.out.println(N - ans);
+	}
+
+}
