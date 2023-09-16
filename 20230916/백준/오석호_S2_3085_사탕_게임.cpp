fb6e86be28cc1d93deb1b668e9d7fd2334b150ec
Ssoco <dlscjs8646@gmail.com>
2023-09-16 10:49:31 +0900

    Update 오석호_S2_3085_사탕_게임.cpp

--- 20230916/백준/오석호_S2_3085_사탕_게임.cpp
+++ 20230916/백준/오석호_S2_3085_사탕_게임.cpp
@@ -53,24 +53,31 @@ int main() {
 		}
 	}
 
+	// 기본상태에서 확인
+	chk();
+
 	// 가로 바꾸기
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n - 1; ++j) {
-			// 변경
-			swap(arr[i][j], arr[i][j + 1]);
-			// 확인
-			chk();
-			// 복구
-			swap(arr[i][j], arr[i][j + 1]);
+			if (arr[i][j] != arr[i][j + 1]) {
+				// 변경
+				swap(arr[i][j], arr[i][j + 1]);
+				// 확인
+				chk();
+				// 복구
+				swap(arr[i][j], arr[i][j + 1]);
+			}
 		}
 	}
 
 	// 세로 바꾸기
 	for (int i = 0; i < n - 1; ++i) {
 		for (int j = 0; j < n; ++j) {
-			swap(arr[i + 1][j], arr[i][j]);
-			chk();
-			swap(arr[i + 1][j], arr[i][j]);
+			if (arr[i][j] != arr[i + 1][j]) {
+				swap(arr[i + 1][j], arr[i][j]);
+				chk();
+				swap(arr[i + 1][j], arr[i][j]);
+			}
 		}
 	}
 
