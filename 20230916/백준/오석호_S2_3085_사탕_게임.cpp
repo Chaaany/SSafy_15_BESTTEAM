75e03571b590dc96b537fee2bb2462acc68cdc35
Ssoco <dlscjs8646@gmail.com>
2023-09-16 10:46:50 +0900

    Create 오석호_S2_3085_사탕_게임.cpp

---
+++ 20230916/백준/오석호_S2_3085_사탕_게임.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+char arr[51][51];
+int ans = 0;
+
+void chk() {
+	// 가로 체크
+	for (int i = 0; i < n; ++i) {
+		int tmp = 1;
+		for (int j = 0; j < n - 1; ++j) {
+			if (arr[i][j] == arr[i][j + 1]) {
+				++tmp;
+				ans = max(ans, tmp);
+			}
+			else {
+				ans = max(ans, tmp);
+				tmp = 1;
+			}
+		}
+	}
+
+	// 세로 체크
+	for (int j = 0; j < n; ++j) {
+		int tmp = 1;
+		for (int i = 0; i < n - 1; ++i) {
+			if (arr[i][j] == arr[i + 1][j]) {
+				++tmp;
+				ans = max(ans, tmp);
+			}
+			else {
+				ans = max(ans, tmp);
+				tmp = 1;
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 3085 사탕 게임
+	// 브루트포스
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cin >> arr[i][j];
+		}
+	}
+
+	// 가로 바꾸기
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n - 1; ++j) {
+			// 변경
+			swap(arr[i][j], arr[i][j + 1]);
+			// 확인
+			chk();
+			// 복구
+			swap(arr[i][j], arr[i][j + 1]);
+		}
+	}
+
+	// 세로 바꾸기
+	for (int i = 0; i < n - 1; ++i) {
+		for (int j = 0; j < n; ++j) {
+			swap(arr[i + 1][j], arr[i][j]);
+			chk();
+			swap(arr[i + 1][j], arr[i][j]);
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
