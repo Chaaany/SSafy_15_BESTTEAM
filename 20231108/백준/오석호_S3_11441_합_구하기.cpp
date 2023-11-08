224286b0bf1337377171a0a7c976291090f5ef1d
Ssoco <dlscjs8646@gmail.com>
2023-11-09 08:23:22 +0900

    Update 오석호_S3_11441_합_구하기.cpp

--- 20231108/백준/오석호_S3_11441_합_구하기.cpp
+++ 20231108/백준/오석호_S3_11441_합_구하기.cpp
@@ -7,23 +7,23 @@ int main() {
 	cout.tie(NULL);
 
 	// 11441 합 구하기
-  // 누적합
-  int n, a, m, ii, jj;
-  int arr[100001] = { 0 };
-  cin >> n;
-
-  int sum = 0;
-  for (int i = 0; i < n; ++i) {
-      cin >> a;
-      sum += a;
-      arr[i + 1] = sum;
-  }
-  
-  cin >> m;
-  for (int i = 0; i < m; ++i) {
-      cin >> ii >> jj;
-      cout << arr[jj] - arr[ii - 1] << '\n';
-  }
+	// 누적합
+	int n, a, m, ii, jj;
+	int arr[100001] = { 0 };
+	cin >> n;
+	
+	int sum = 0;
+	for (int i = 0; i < n; ++i) {
+	cin >> a;
+	sum += a;
+	arr[i + 1] = sum;
+	}
+	
+	cin >> m;
+	for (int i = 0; i < m; ++i) {
+	cin >> ii >> jj;
+	cout << arr[jj] - arr[ii - 1] << '\n';
+	}
 
 	return 0;
 }
