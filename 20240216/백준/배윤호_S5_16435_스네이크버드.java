a92dfa8d891f2498e0f52e998e3f06fdfa2ad960
NKLCBAE <87966378+NKLCBAE@users.noreply.github.com>
2024-02-17 09:29:05 +0900

    Create 배윤호_S5_16435_스네이크버드.java

---
+++ 20240216/백준/배윤호_S5_16435_스네이크버드.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < N; i++) {
+			if (L >= arr[i]) {
+				L++;
+			} else {
+				break;
+			}
+		}
+		System.out.println(L);
+	}
+
+}
