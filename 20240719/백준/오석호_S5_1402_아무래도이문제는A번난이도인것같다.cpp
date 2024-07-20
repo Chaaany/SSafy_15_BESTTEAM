bf56dff45c8da031d1bb6d00b17b8a937d40ea64
Ssoco <dlscjs8646@gmail.com>
2024-07-20 14:32:09 +0900

    Create 오석호_S5_1402_아무래도이문제는A번난이도인것같다.cpp

---
+++ 20240719/백준/오석호_S5_1402_아무래도이문제는A번난이도인것같다.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 1402 아무래도이문제는A번난이도인것같다
+	// 수학, 애드혹
+	int t, a, b;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		cin >> a >> b;
+		cout << "yes" << '\n';
+	}
+
+	return 0;
+}
