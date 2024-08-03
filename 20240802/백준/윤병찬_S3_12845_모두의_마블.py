68a23800a53bf700262bab280a4f2a7cd7650fc8
윤병찬/Enterprise DAX TF <ben125@lguplus.co.kr/GitHubPrivateEmail>
2024-08-03 09:25:09 +0900

    Create 윤병찬_S3_12845_모두의_마블.py

---
+++ 20240802/백준/윤병찬_S3_12845_모두의_마블.py
@@ -0,0 +1,11 @@
+n = int(input())
+level_list = list(map(int, input().split()))
+level_list.sort(reverse=True)
+ans = 0
+
+for i in range(len(level_list)-1):
+    i = 0
+    ans += level_list[i] + level_list[i+1]
+    del level_list[i+1]
+ 
+print(ans)
