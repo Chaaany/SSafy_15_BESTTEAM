b1734c9bb0c5d4f3dc026e1aa2e74138e0d018a0
윤병찬/Enterprise DAX TF <ben125@lguplus.co.kr/GitHubPrivateEmail>
2024-08-10 10:31:17 +0900

    Create 20240810

---
+++ 20240810/백준/윤병찬_S3_10451_순열_사이클.py
@@ -0,0 +1,25 @@
+# dictionary로 값 만들기
+
+count = 0
+
+def check_permutation_cycle(dict_value, current_node, visited):
+    if visited[current_node]:
+        return 1
+    
+    visited[current_node] = True
+    return check_permutation_cycle(dict_value, dict_value[current_node], visited)
+    
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    count = 0
+    dict_value = {}
+    keys = list(map(int, input().split()))
+    visited = [False] * (N + 1)
+    for value in range(1, N + 1):
+        dict_value[value] = keys[value - 1]
+    for current_node in range(1, N + 1):
+        if not visited[current_node]:
+            count += check_permutation_cycle(dict_value, current_node, visited)
+    print(count)
