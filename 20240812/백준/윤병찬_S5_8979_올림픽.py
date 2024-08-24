5a90df307ff22cbfa88f82ba6050a1b0fa838559
Chaaany <ben125@lguplus.co.kr>
2024-08-24 15:41:59 +0900

    Change 윤병찬_S5_8979_올림픽.py

--- 20240812/백준/윤병찬_S5_8979_올림픽.py
+++ 20240812/백준/윤병찬_S5_8979_올림픽.py
@@ -9,7 +9,7 @@ for _ in range(N):
     tmp_received_medal = list(map(int, input().split(' ')))
     received_medal_list.append(tmp_received_medal)
 
-received_medal_list.sort(key=lambda x: (-x[0], -x[1], -x[2]))
+received_medal_list.sort(key=lambda x: (-x[1], -x[2], -x[3]))
 
 target_index = 0
 for i in range(N):
@@ -17,10 +17,12 @@ for i in range(N):
         target_index = i
 
 rank = 1
-for i in range(target_index):
-    if not(received_medal_list[i][1] == received_medal_list[target_index][1] and received_medal_list[i][2] == received_medal_list[target_index][2]):
-        rank += 1
-
-print(rank)
-
-
+received_medal_list[0].append(rank)
+for i in range(1, N):
+    rank += 1
+    if received_medal_list[i - 1][1] == received_medal_list[i][1] and received_medal_list[i - 1][2] == received_medal_list[i][2] and received_medal_list[i - 1][3] == received_medal_list[i][3]:
+        received_medal_list[i].append(received_medal_list[i - 1][4])
+    else:
+        received_medal_list[i].append(rank)
+    
+print(received_medal_list[target_index][4])
