0b6fcf819dc22b55dbad756c3d41e17a074b7e27
Chaaany <ben125@lguplus.co.kr>
2024-09-20 22:48:36 +0900

    Change 윤병찬_S4_25375_아주_간단한_문제.py

--- 20240918/백준/윤병찬_S4_25375_아주_간단한_문제.py
+++ 20240918/백준/윤병찬_S4_25375_아주_간단한_문제.py
@@ -3,7 +3,7 @@
 # x = ar1, y = ar2 = b - x = b - ar1 = a(r1 + r2) - ar1
 # r1, r2는 서로소
 # a(r1 + r2) = b => r1 + r2 = b/a -> r1 + r2 >= 2(자연수의 합), b/a = b는 a의 배수
-t = int(input())  # 테스트 케이스 수 입력
+t = int(input())
 
 for _ in range(t):
     a, b = map(int, input().split())
