02267572527d9adc260c3b82e24a9721a8f2042e
Chaaany <ben125@lguplus.co.kr>
2024-09-28 10:11:09 +0900

    Change 윤병찬_S3_4375_1.py

--- 20240928/백준/윤병찬_S3_4375_1.py
+++ 20240928/백준/윤병찬_S3_4375_1.py
@@ -1,5 +1,11 @@
 def find_number(n):
-    pass
+    num = 1
+    count = 1
+
+    while num % n != 0:
+        num = (num * 10 + 1) % n
+        count += 1
+    return count
 
 while True:
     try:
