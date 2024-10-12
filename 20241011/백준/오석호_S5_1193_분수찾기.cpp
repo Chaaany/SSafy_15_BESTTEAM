f58d225279c6a480889e567f8aa98319e4f6a283
Ssoco <dlscjs8646@gmail.com>
2024-10-12 09:33:50 +0900

    Create 오석호_S5_1193_분수찾기.cpp

---
+++ 20241011/백준/오석호_S5_1193_분수찾기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+	  // 1193 분수찾기
+    // 수학
+
+    int x;
+    cin >> x;
+
+    int cnt = 0;
+    int tmp = 0;
+    while (x > tmp) {
+        tmp += cnt + 1;
+        ++cnt;
+    }
+    if (cnt % 2 == 0) cout << cnt - (tmp - x) << '/' << 1 + (tmp - x);
+    else cout << 1 + (tmp - x) << '/' << cnt - (tmp - x);
+
+    return 0;
+}
