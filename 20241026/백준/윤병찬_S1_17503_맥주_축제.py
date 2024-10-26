6d19f833e21f774dc2c572362f70c3f417b43091
윤병찬/Enterprise DAX TF <ben125@lguplus.co.kr/GitHubPrivateEmail>
2024-10-26 11:15:16 +0900

    Create 윤병찬_S1_17503_맥주_축제.py

---
+++ 20241026/백준/윤병찬_S1_17503_맥주_축제.py
@@ -0,0 +1,28 @@
+import heapq
+
+N, M, K = list(map(int, input().split()))
+beer_list = []
+for _ in range(K):
+    V, C = map(int, input().split())
+    beer_list.append([C, V])
+
+heapq.heapify(beer_list)
+
+drunk_beer_list = []
+sum_K = 0
+
+for _ in range(K):
+    value_temp = heapq.heappop(beer_list)
+    C, V = value_temp[0], value_temp[1]
+    sum_K += V
+    heapq.heappush(drunk_beer_list, V)
+
+    if len(drunk_beer_list) > N:
+        value = heapq.heappop(drunk_beer_list)
+        sum_K -= value
+
+    if len(drunk_beer_list) == N and sum_K >= M:
+        print(C)
+        exit(0)    
+
+print(-1)
