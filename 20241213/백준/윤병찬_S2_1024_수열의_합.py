eabdfa6e6ec046727cec0cabd1dffe1d8d143a3f
Chaaany <ben125@lguplus.co.kr>
2024-12-14 09:40:05 +0900

    Change 윤병찬_S2_1024_수열의_합.py

--- 20241213/백준/윤병찬_S2_1024_수열의_합.py
+++ 20241213/백준/윤병찬_S2_1024_수열의_합.py
@@ -6,6 +6,27 @@
 # i * (i - 1) / 2 - j * (j - 1) / 2 = N
 #  2 =< L = i - j +1 <= 100
 
+def find_sequence(N, L):
+    for length in range(L, 101):  # 최소 L에서 최대 100까지 시도
+        # 연속된 자연수의 합 공식: (x + x+1 + ... + x+(length-1)) = length * x + (0+1+...+(length-1))
+        # 따라서, x = (N - length*(length-1)//2) / length
+        x = (N - (length * (length - 1)) // 2) / length
+        if x.is_integer() and x >= 0:
+            x = int(x)
+            return list(range(x, x + length))
+    return -1  # 찾을 수 없으면 -1 반환
+
+# 입력 받기
+N, L = map(int, input().split())
+
+# 결과 출력
+result = find_sequence(N, L)
+if result == -1:
+    print(-1)
+else:
+    print(' '.join(map(str, result)))
+
+
 
 # 1. 수열 생성 가능 여부 확인
 # 홀수로는 나머지가 없고, 나누어떨어지면 되고, 짝수로는 .5로 나누어떨어져야 함 
@@ -31,3 +52,4 @@ for i in range(L, min(101, N)):
             print(j, end=" ")
         exit(0)
 print(-1)
+
