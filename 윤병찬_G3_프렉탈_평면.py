a7e90e09fff90aec63da323c77f6c55da262dd2f
Chaaany <ben125@lguplus.co.kr>
2024-12-21 11:04:43 +0900

    Change 윤병찬_G3_프렉탈_평면.py

--- 윤병찬_G3_프렉탈_평면.py
+++ 윤병찬_G3_프렉탈_평면.py
@@ -1,21 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def check_black(l, point):
+def check(side_length, point):
     x,y = point
-    center = l//N 
-    if l == 1:
+    center = side_length//N 
+    if side_length == 1:
         return 0
-    if center * (N-K)//2 <= x < center * (N+K)//2 and center * (N-K)//2 <= y < center * (N+K)//2:
+    if center * ( N - K ) // 2 <= x < center * ( N + K ) // 2 and center * ( N - K ) // 2 <= y < center * ( N + K ) // 2:
         return 1
+    
     x %= center
     y %= center
-    return check_black(l//N, [x,y])
+    
+    return check(side_length//N, [x,y])
 s,N,K,R1,R2,C1,C2 = map(int,input().split())
 side_length = N**s
 
 
 for i in range(R1, R2+1):
     for j in range(C1, C2+1):
-        print(check_black(l,[i,j]), end="")
+        print(check(side_length,[i,j]), end="")
     print()
