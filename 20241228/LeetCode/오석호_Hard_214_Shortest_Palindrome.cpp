c996111993928bb3c6596fcc7483c399d439ba62
Ssoco <dlscjs8646@gmail.com>
2024-12-28 12:58:48 +0900

    Update 오석호_Hard_214_Shortest_Palindrome.cpp

--- 20241228/LeetCode/오석호_Hard_214_Shortest_Palindrome.cpp
+++ 20241228/LeetCode/오석호_Hard_214_Shortest_Palindrome.cpp
@@ -1,19 +1,24 @@
+#include <iostream>
+using namespace std;
+
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int idx = 0;
-        // 앞에서부터 팰린드롬인 영역 찾기
-        for(int i = 0; i < s.size(); ++i) {
-            if(s[idx] == s[s.size() - i - 1]) ++idx;
-        }
-        // 만약 그 자체로 팰린드롬이면 그대로 출력
-        if(idx == s.size()) return s;
+        // s의 역순 문자열을 생성
+        string rev = s;
+        reverse(rev.begin(), rev.end());
 
-        // 팰린드롬이 아닌 영역 구하기
-        string res = s.substr(idx, s.size());
-        string reverse_res = res;
-        reverse(reverse_res.begin(), reverse_res.end());
-    
-        return reverse_res + shortestPalindrome(s.substr(0, idx)) + res;
+        for (int i = 0; i < rev.size(); ++i) {
+            // s의 0번 인덱스부터 (s.size() - i)번 인덱스 전까지랑
+            // rev의 i번 인덱스부터 끝까지랑 비교해서 같으면
+            // rev의 0번 인덱스부터 i번 인덱스까지랑 s를 더한 값을 출력한다.
+            if (s.compare(0, s.size() - i, rev, i) == 0) {
+                return rev.substr(0, i) + s;
+            }
+        }
+
+        // for문에서 return이 안되는 케이스는 s가 공백인 케이스밖에 없다
+        // 이 때는 그대로 출력한다.
+        return "";
     }
 };
